a95cc1a82e95b09fc663fbf0ac202a09a74f38ae
shennian <shen.n1993@gmail.com>
2016-03-27 23:47:30 +0800

    20160327

---
+++ flask/database_test/mysql/mysql.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sen_flask_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    gender = db.Column(db.String(5))
+    password = db.Column(db.String(255), unique=True)
+
+
+@app.route('/')
+def hello_world():
+    user = User(name="sen", id=1, email="hha", gender='233', password='234')
+    db.session.add(user)
+    db.session.commit()
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
