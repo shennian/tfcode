ca0da591770f3560b7ac05043a3ceb2ab24b4b28
shennian <shen.n1993@gmail.com>
2016-04-13 23:21:39 +0800

    20160413

--- flask/my-zhihu/app/main/views.py
+++ flask/my-zhihu/app/main/views.py
@@ -4,9 +4,9 @@ from flask import render_template, session, redirect, url_for
 from . import main
 from .forms import NameForm
 from .. import db
-from ..models import User
+from ..models import User, Answer
 
-
+'''
 @main.before_request
 def haha():
     session_id = None
@@ -14,12 +14,19 @@ def haha():
         session_id = session['id']
     except:
         return 'login first'
+'''
 
 
 @main.route("/main", methods=['get', 'post'])
 def index():
     print 'shit'
-    return render_template("main/main.html")
+
+    answers = Answer.query.order_by(Answer.time.desc()).all()
+    '''
+    for answer in answers:
+        answer.answer_content
+    '''
+    return render_template("main/main.html", posts=answers)
 
 
 @main.route('/main/vote', methods=['get', 'post'])
